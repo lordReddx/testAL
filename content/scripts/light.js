75b52d1f9d99cccf29d4efec0b0a17cb61f5d86d
lordReddx <aleksandr.doreckii@yandex.ru>
2020-06-26 21:40:57 +0300

    code

--- content/scripts/light.js
+++ content/scripts/light.js
@@ -1,9 +1,9 @@
 const light = extendContent(Light);
-    Draw.color(Color.valueOf("00FF0061")
+    mainFrame.setBackground(Color.white)
 
 
 
-
+//Draw.color(Color.valueOf("00FF0061")
 //mainFrame.setBackground(Color.white);
 //Draw.color(Color.white, Color.lightGray, e.fin())
 //color:#00FF0061

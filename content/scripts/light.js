911968eac3d2eece207b3264bfe5336a831e633d
lordReddx <aleksandr.doreckii@yandex.ru>
2020-06-26 21:05:03 +0300

    code

--- content/scripts/light.js
+++ content/scripts/light.js
@@ -1,1 +1,23 @@
-Draw.color(Color.white, Color.lightGray, e.fin())
+    public void add(float x, float y, float radius, Color color, float opacity){
+        if(!enabled()) return;
+
+        float res = color.toFloatBits();
+        add(() -> {
+            Draw.color(res);
+            Draw.alpha(opacity);
+            Draw.rect("circle-shadow", x, y, radius * 2, radius * 2);
+        });
+    }
+
+    public void add(float x, float y, TextureRegion region, Color color, float opacity){
+        if(!enabled()) return;
+
+        float res = color.toFloatBits();
+        add(() -> {
+            Draw.color(res);
+            Draw.alpha(opacity);
+            Draw.rect(region, x, y);
+        });
+    }
+//mainFrame.setBackground(Color.white);
+//Draw.color(Color.white, Color.lightGray, e.fin())

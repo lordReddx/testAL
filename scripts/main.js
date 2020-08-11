15a18f2ab83eda4fa7709649a737c227eabb1519
lordReddx <aleksandr.doreckii@yandex.ru>
2020-08-11 15:39:01 +0300

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-require("blocks/crafting/chemicalplant");
+require("chemicalplant");
+require("multi-test");
+//you have to add mod-name like this to stop using multi-lib and enable your mod scripts
+require("multi-lib-example/multi-test2");

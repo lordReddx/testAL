831d9b1ba4374b88afbac7a333b0b6f93d17ef95
lordReddx <aleksandr.doreckii@yandex.ru>
2021-01-21 19:38:04 +0300

    test

--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-//require("chemicalplant");
-//require("multi-test");
-//you have to add mod-name like this to stop using multi-lib and enable your mod scripts
-//require("multi-lib-example/multi-test2");

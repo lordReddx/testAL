ae5ad0d1ae45bd7e4a62c70833d99ce3c45fa41b
lordReddx <aleksandr.dvoreckii@yandex.ru>
2021-03-25 19:42:38 +0300

    Test

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,19 +1,19 @@
 Events.on(ClientLoadEvent, () => {
-    const routros = new Planet("routros", Planets.sun, 1, 1);
-    routros.generator = new SerpuloPlanetGenerator();
-    routros.mesh = new HexMesh(routros, 5);
-    routros.orbitRadius = 10;
-    routros.orbitTime = 1.5 * 60;
-    routros.rotateTime = 30;
-    routros.bloom = true;
-    routros.accessible = true;
-    routros.startSector = 3;
-    routros.hasAtmosphere = true;
-    routros.atmosphereColor = Liquids.cryofluid.color;
-    routros.atmosphereRadIn = 0.1;
-    routros.atmosphereRadOut = 0.3;
-    routros.alwaysUnlocked = true;
-    routros.localizedName = "Router";
+    const mino = new Planet("mino", Planets.sun, 1, 1);
+    mino.generator = new SerpuloPlanetGenerator();
+    mino.mesh = new HexMesh(mino, 5);
+    mino.orbitRadius = 10;
+    mino.orbitTime = 1.5 * 60;
+    mino.rotateTime = 30;
+    mino.bloom = true;
+    mino.accessible = true;
+    mino.startSector = 3;
+    mino.hasAtmosphere = true;
+    mino.atmosphereColor = Liquids.cryofluid.color;
+    mino.atmosphereRadIn = 0.1;
+    mino.atmosphereRadOut = 0.3;
+    mino.alwaysUnlocked = true;
+    mino.localizedName = "Mino";
 
     
 const plains = new SectorPreset("router1", mino, 1);

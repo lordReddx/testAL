e23cfbb443d35c39ab8b97e8bef3cdf5b7c81f5b
lordReddx <aleksandr.dvoreckii@yandex.ru>
2021-03-25 19:28:53 +0300

    Test

--- scripts/planets.js
+++ scripts/planets.js
@@ -15,9 +15,31 @@ Events.on(ClientLoadEvent, () => {
     routros.alwaysUnlocked = true;
     routros.localizedName = "Router";
 
-    /*for(var i = 0; i < 5; i++){
-        var h = new SectorPreset("router" + i, routros, i);
-        h.localizedName = "Router" + i;
-        h.alwaysUnlocked = true;
-    };*/
+    
+const plains = new SectorPreset("router1", mino, 1);
+plains.captureWave = 45;
+plains.localizedName = "n2";
+plains.difficulty = 8;
+plains.alwaysUnlocked = false;
+
+const oldRefinery = new SectorPreset("router2", mino, 2);
+oldRefinery.localizedName = "n3";
+oldRefinery.difficulty = 8;
+oldRefinery.alwaysUnlocked = false;
+
+const frozenMountains = new SectorPreset("router3", mino, 3);
+frozenMountains.localizedName = "n1";
+frozenMountains.difficulty = 5;
+frozenMountains.alwaysUnlocked = true;
+
+const spaceshipWorkshop = new SectorPreset("router4", mino, 4);
+spaceshipRepairShop.captureWave = 30;
+spaceshipRepairShop.localizedName = "n4";
+spaceshipRepairShop.difficulty = 5;
+spaceshipRepairShop.alwaysUnlocked = false;
+
+const spaceAirport = new SectorPreset("router5", mino, 5);
+spaceAirport.localizedName = "n5";
+spaceAirport.difficulty = 9;
+spaceAirport.alwaysUnlocked = false;
 });

2aa38edf5fa339a21bb5dc7f9208ba80f561186d
lordReddx <aleksandr.dvoreckii@yandex.ru>
2021-03-25 18:57:15 +0300

    Test

--- scripts/sectorPresets.js
+++ scripts/sectorPresets.js
@@ -1,27 +1,27 @@
 
-const plains = new SectorPreset("sector1", mino, 1);
+const plains = new SectorPreset("router1", mino, 1);
 plains.captureWave = 45;
 plains.localizedName = "n2";
 plains.difficulty = 8;
 plains.alwaysUnlocked = false;
 
-const oldRefinery = new SectorPreset("sector2", mino, 2);
+const oldRefinery = new SectorPreset("router2", mino, 2);
 oldRefinery.localizedName = "n3";
 oldRefinery.difficulty = 8;
 oldRefinery.alwaysUnlocked = false;
 
-const frozenMountains = new SectorPreset("sector3", mino, 3);
+const frozenMountains = new SectorPreset("router3", mino, 3);
 frozenMountains.localizedName = "n1";
 frozenMountains.difficulty = 5;
 frozenMountains.alwaysUnlocked = false;
 
-const spaceshipWorkshop = new SectorPreset("n4", mino, 4);
+const spaceshipWorkshop = new SectorPreset("router4", mino, 4);
 spaceshipRepairShop.captureWave = 30;
-spaceshipRepairShop.localizedName = "Spaceship workshop";
+spaceshipRepairShop.localizedName = "n4";
 spaceshipRepairShop.difficulty = 5;
 spaceshipRepairShop.alwaysUnlocked = false;
 
-const spaceAirport = new SectorPreset("sector5", mino, 5);
+const spaceAirport = new SectorPreset("router5", mino, 5);
 spaceAirport.localizedName = "n5";
 spaceAirport.difficulty = 9;
 spaceAirport.alwaysUnlocked = false;
